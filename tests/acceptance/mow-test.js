d734af92ab409640c2d3c3d64b24ecd481c21669
toran billups <toranb@gmail.com>
2015-11-30 17:20:38 -0600

    added example acceptance tests for speed work

---
+++ tests/acceptance/mow-test.js
@@ -0,0 +1,155 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'katie/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | mow');
+
+test('1 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('2 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('3 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('4 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('5 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('6 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('7 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('8 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('9 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('10 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('11 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('12 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('13 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('14 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('15 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('16 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('17 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('18 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('19 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('20 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('21 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('22 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('23 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('24 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+test('25 visiting nothing does nothing', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
